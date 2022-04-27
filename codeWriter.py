64d061c3e96f70645b49d25fda428428990c162b
Cody <cody.tian@icloud.com>
2022-04-27 14:32:34 -0700

    open VM and ASM files

---
+++ codeWriter.py
@@ -0,0 +1,6 @@
+
+class CodeWriter:
+    def __init__(self):
+        pass
+
+
---
+++ main.py
@@ -0,0 +1,11 @@
+vm = open('StackArithmetic/StackTest/StackTest.vm', 'r')
+asm = open('StackArithmetic/StackTest/StackTest.asm', 'w')
+
+
+for code in vm:
+    print(vm.readline())
+
+
+
+
+
